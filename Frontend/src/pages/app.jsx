b497e176ca03ec773e27a4ce662c4212d2788663
shubhamc <sh572302@dal.ca>
2023-06-16 14:21:48 -0300

    Mergerd with main

--- Frontend/src/pages/app.jsx
+++ Frontend/src/pages/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavbarComp from "../Components/navbar/navbarComp";
+import "../app.css";
 
 const App = () => {
   return (

38ea38a4e65811e5b896c06a1186019fb09ff810
Aadith Shameel <aadith@Aadiths-MacBook-Pro.local>
2023-06-17 03:21:38 -0300

    Added Earn More Component. Added corresponsding images in assests folder

--- Frontend/src/pages/landing/landingPage.jsx
+++ Frontend/src/pages/landing/landingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import StartComp from "../../Components/landing/startComp/startComp";
+import EarnMoreComp from "../../Components/landing/earnMore/earnMoreComp";
 import "./landingPage.css";
 
 function LandingPage() {
   return (
     <div className="landingPage">
       <StartComp />
+      <EarnMoreComp />
     </div>
   );
 }

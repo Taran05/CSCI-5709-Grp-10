8b1ab8638f352a69a448a23e45327908cb424169
Amanjot Singh <am854663@dal.ca>
2023-07-26 01:53:06 -0300

    Added getUserDetail API

--- Backend/Dev/routes/userAuthentication/userRegistrationRoute.ts
+++ Backend/Dev/routes/userAuthentication/userRegistrationRoute.ts
@@ -2,12 +2,13 @@ import express from "express";
 const router = express.Router();
 import userRegistrationCont from "../../controllers/userAuthentication/userRegistrationCont";
 import getAllUsernamesCont from "../../controllers/userAuthentication/getAllUsernamesCont";
-
+import getUserDetailsCont from "../../controllers/userAuthentication/getUserDetailsCont";
 // POST /register/
 router.post("/registerUser", userRegistrationCont.userRegisteration);
 router.get(
   "/registerUser/getAllUsernames",
   getAllUsernamesCont.getAllUsernames
 );
+router.post("/registerUser/getUserDetails", getUserDetailsCont.getUserDetails);
 
 export const userRegisterRoute = router;

6858ce9892ab52e67c15468874407dcb8f8fc8fb
shubhamc <sh572302@dal.ca>
2023-07-26 04:05:20 -0300

    added snackbar

--- Frontend/src/pages/queries/queriesPage.jsx
+++ Frontend/src/pages/queries/queriesPage.jsx
@@ -8,7 +8,7 @@ import "./queriesPage.css";
 import { Grid } from "@mui/material";
 import axios from "axios";
 import { GET_QUERY } from "../../utils/apiUrls";
-
+import { SnackbarProvider } from "notistack";
 import { useNavigate } from "react-router-dom";
 
 function QueriesPage(props) {
@@ -85,54 +85,56 @@ function QueriesPage(props) {
   );
 
   return (
-    <div className="pageBody">
-      <div className="header">
-        {/* <HeaderComp
+    <SnackbarProvider maxSnack={3}>
+      <div className="pageBody">
+        <div className="header">
+          {/* <HeaderComp
           changeDisplayOption={changeDisplayOption}
           displayOption={displayOption}
         /> */}
 
-        <PageHeaderComp
-          pageTitle="Queries"
-          changeDisplayOption={changeDisplayOption}
-          displayOption={displayOption}
-        />
-      </div>
+          <PageHeaderComp
+            pageTitle="Queries"
+            changeDisplayOption={changeDisplayOption}
+            displayOption={displayOption}
+          />
+        </div>
 
-      <div className="belowHeader container">
-        <Grid container spacing={1}>
-          <Grid item xs={3}>
-            <div className="side-bar item">
-              <SidebarComp
-                handleUserClick={handleUserClick}
-                selectedUserId={value}
-                displayOption={displayOption}
-                Queries={Queries}
-                setPendingQueryCount={setPendingQueryCount}
-                setAnsweredQueryCount={setAnsweredQueryCount}
-              />
-            </div>
-          </Grid>
+        <div className="belowHeader container">
+          <Grid container spacing={1}>
+            <Grid item xs={3}>
+              <div className="side-bar item">
+                <SidebarComp
+                  handleUserClick={handleUserClick}
+                  selectedUserId={value}
+                  displayOption={displayOption}
+                  Queries={Queries}
+                  setPendingQueryCount={setPendingQueryCount}
+                  setAnsweredQueryCount={setAnsweredQueryCount}
+                />
+              </div>
+            </Grid>
 
-          <Grid item xs={9}>
-            <div className="body item">
-              <BodyComp
-                userId={value}
-                selectedUserId={value}
-                updateQueries={updateQueries}
-                Queries={Queries}
-                changeDisplayOption={changeDisplayOption}
-                displayOption={displayOption}
-                pendingQueryCount={pendingQueryCount}
-                answeredQueryCount={answeredQueryCount}
-                setPendingQueryCount={setPendingQueryCount}
-                setAnsweredQueryCount={setAnsweredQueryCount}
-              />
-            </div>
+            <Grid item xs={9}>
+              <div className="body item">
+                <BodyComp
+                  userId={value}
+                  selectedUserId={value}
+                  updateQueries={updateQueries}
+                  Queries={Queries}
+                  changeDisplayOption={changeDisplayOption}
+                  displayOption={displayOption}
+                  pendingQueryCount={pendingQueryCount}
+                  answeredQueryCount={answeredQueryCount}
+                  setPendingQueryCount={setPendingQueryCount}
+                  setAnsweredQueryCount={setAnsweredQueryCount}
+                />
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
+    </SnackbarProvider>
   );
 }
 

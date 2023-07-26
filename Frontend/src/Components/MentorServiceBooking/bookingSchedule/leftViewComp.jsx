4e09c51e027669c5e89d5bf48d85e5280e97ce30
slakhanpal <sh475218@dal.ca>
2023-07-26 01:38:31 -0300

    Updated left component of the booking schedule

--- Frontend/src/Components/MentorServiceBooking/bookingSchedule/leftViewComp.jsx
+++ Frontend/src/Components/MentorServiceBooking/bookingSchedule/leftViewComp.jsx
@@ -1,60 +1,30 @@
 import React from "react";
-import { Paper, Typography, Divider, Box } from "@mui/material";
+import { Paper, Typography, Box } from "@mui/material";
 import EventIcon from "@mui/icons-material/Event";
-import { styled } from "@mui/system";
-
-const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(4),
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-}));
-
-const StyledHeader = styled(Box)(({ theme }) => ({
-  marginBottom: theme.spacing(2),
-  backgroundColor: "#e3e3e3",
-  padding: theme.spacing(2),
-}));
-
-const StyledFooter = styled(Box)(({ theme }) => ({
-  marginTop: theme.spacing(4),
-  paddingTop: theme.spacing(2),
-  borderTop: `1px solid ${theme.palette.divider}`,
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-}));
-
-const DividerVertical = styled("div")({
-  height: "1em",
-  width: "1px",
-  backgroundColor: "#000",
-  margin: "0 1em",
-});
+import "./leftViewComponent.css"; // Import the CSS file
 
 const LeftViewComponent = ({ serviceName, serviceDuration, servicePrice }) => {
   return (
-    <StyledPaper elevation={3}>
+    <Paper elevation={3} className="left-view-component">
       <div>
-        <StyledHeader>
+        <Box className="left-view-component-header">
           <Typography variant="h3" gutterBottom component="div">
             Shivam Lakhanpal
           </Typography>
           <Typography variant="h5" color="textSecondary" gutterBottom>
             {serviceName}
           </Typography>
-        </StyledHeader>
+        </Box>
       </div>
-      <StyledFooter>
+      <Box className="left-view-component-footer">
         <Typography variant="h6">{servicePrice} CAD</Typography>
-        <DividerVertical />
+        <div className="divider-vertical" />
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <EventIcon sx={{ fontSize: "1.2rem", marginRight: "4px" }} />
           <Typography variant="h6">{serviceDuration} of meeting</Typography>
         </Box>
-      </StyledFooter>
-    </StyledPaper>
+      </Box>
+    </Paper>
   );
 };
 

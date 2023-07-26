160b47a11b2f2edb332d89916d75ca23f5bf8d0a
Taranjot Singh <tr548284@dal.ca>
2023-07-26 11:46:30 -0300

    chore: 🔨 added the GET API for calendar-settings

--- Backend/Dev/routes/calendarsettingsRoute.ts
+++ Backend/Dev/routes/calendarsettingsRoute.ts
@@ -7,16 +7,38 @@ router.post('/api/saveCalendarSettings', async (req: Request, res: Response) => 
   const calendarSettingsData: ICalendarSettings = req.body;
   console.log(calendarSettingsData);
   try {
-    const calendarSettings: ICalendarSettings = new CalendarSettings({
-      calendarSettingsData,
-    });
-
-    await calendarSettings.save();
-
-    res.status(201).json({ message: 'Calendar Settings saved successfully.' });
+    const existingSettings = await CalendarSettings.findOne();
+    console.log(existingSettings);
+    if(existingSettings){
+      existingSettings.calendarSettingsData = calendarSettingsData;
+      await existingSettings.save();
+      res.status(200).json({ message: 'Calendar Settings Updated Successfully.' });
+    }
+    else{
+      const calendarSettings: ICalendarSettings = new CalendarSettings({
+        calendarSettingsData,
+      });
+      await calendarSettings.save();
+    }  
+    res.status(201).json({ message: 'Calendar Settings Saved Successfully.' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to save Calendar Settings' });
+  }
+});
+
+router.get('/api/getCalendarSettings', async (_req: Request, res: Response) => {
+  try {
+    const calendarSettings: ICalendarSettings[] = await CalendarSettings.find();
+    if (calendarSettings.length > 0) {
+      const settings = calendarSettings[0];
+      res.status(200).json({ calendarSettings: settings });
+    } else {
+      res.status(200).json({ calendarSettings: {} });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to get calendar settings' });
   }
 });
 

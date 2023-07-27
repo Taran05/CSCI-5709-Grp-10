07bc0b0f4737913d2a8193f35b63cb8c521406f2
shubhamc <sh572302@dal.ca>
2023-07-27 18:54:55 -0300

    Updated path

--- Frontend/src/utils/apiUrls.js
+++ Frontend/src/utils/apiUrls.js
@@ -1,5 +1,5 @@
 // const domain = "http://localhost:3001/";
-const domain = "https://learnly-backend-0usq.onrender.com/";
+const domain = "https://learnly-backend-ub6j.onrender.com/";
 
 export const REGISTER_USER = `${domain}api/registerUser`;
 export const LOGIN_USER = `${domain}api/loginUser`;

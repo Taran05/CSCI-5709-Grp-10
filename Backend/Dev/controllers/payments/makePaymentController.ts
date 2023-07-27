4ec6fae6b6683a22889ad034ae79f59296199874
slakhanpal <sh475218@dal.ca>
2023-07-26 21:03:02 -0300

    Added email functionality to StudentBooking

--- Backend/Dev/controllers/payments/makePaymentController.ts
+++ Backend/Dev/controllers/payments/makePaymentController.ts
@@ -1,12 +1,66 @@
-import StudentBooking from "../../models/StudentBooking";
-import { Request as ExpressRequest, Response } from "express";
+/**
+ * @file This file defines the controller for the payment process and the sending of a confirmation email.
+ * @author Shivam Lakhanpal <sh475218@dal.ca/B00932887>
+ */
 
+import nodemailer from "nodemailer";
+import { Request as ExpressRequest, Response } from "express";
+import StudentBooking from "../../models/StudentBooking";
+
+/**
+ * Interface extending the express request to include the mentorId in params.
+ * @interface
+ * @extends {ExpressRequest}
+ * @property {object} params - Express request parameters
+ * @property {string} params.mentorId - The mentor's ID
+ */
 interface RequestWithMentorIdParams extends ExpressRequest {
   params: {
     mentorId: string;
   };
 }
 
+/**
+ * Email configuration for nodemailer.
+ * @constant
+ * @type {object}
+ */
+const emailConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "learnly.io@gmail.com",
+    pass: "mhlqblzgdqrjotzq",
+  },
+};
+
+/**
+ * Async function to send an email using nodemailer.
+ *
+ * @async
+ * @function
+ * @param {any} emailConfig - The email configuration for nodemailer
+ * @param {any} mailOptions - The email message options
+ */
+async function sendEmail(emailConfig: any, mailOptions: any) {
+  try {
+    const transporter = nodemailer.createTransport(emailConfig);
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+}
+
+/**
+ * An express async function to handle the payment process and the sending of a confirmation email.
+ *
+ * @async
+ * @function
+ * @param {RequestWithMentorIdParams} req - Express request object
+ * @param {Response} res - Express response object
+ */
 export const makePayment = async (
   req: RequestWithMentorIdParams,
   res: Response
@@ -14,7 +68,27 @@ export const makePayment = async (
   const mentorId = req.params.mentorId;
 
   try {
+    // Find the relevant bookings
+    const bookings = await StudentBooking.find({ mentorId: mentorId });
+
+    // Extract the student emails
+    const studentEmails = bookings.map((booking) => booking.studentEmail);
+
+    // Perform the payment operation
     await StudentBooking.updateMany({ mentorId: mentorId }, { isPaid: true });
+
+    // Send confirmation email to all students
+    for (let studentEmail of studentEmails) {
+      const mailOptions = {
+        from: "Learnly <learnly.io@gmail.com>",
+        to: studentEmail,
+        subject: `Payment Confirmation`,
+        text: `Your payment for the mentor session has been successful. Thank you for using Learnly.`,
+      };
+
+      await sendEmail(emailConfig, mailOptions);
+    }
+
     res.send({ message: "Payment successful" });
   } catch (error) {
     res.status(500).send({ error: "Payment failed" });

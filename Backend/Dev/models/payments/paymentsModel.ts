6fede05d4e704fc007c1455db7579011bfa6f717
Taranjot Singh <tr548284@dal.ca>
2023-08-11 06:34:09 -0300

    feat: ✨ added the comments for backend code

--- Backend/Dev/models/payments/paymentsModel.ts
+++ Backend/Dev/models/payments/paymentsModel.ts
@@ -1,18 +1,21 @@
 /**
- * @author Taranjot Singh <tr548284@dal.ca/B00945917>
+ * This is the schema definition for the Payment Details model.
+ * Author: Taranjot Singh <tr548284@dal.ca/B00945917>
  */
 
 import mongoose from "mongoose";
 
-const PaymentDetails = mongoose.model(
-  "Payment",
-  new mongoose.Schema({
-    mentorId: String,
-    accountNumber: Number,
-    transitNumber: Number,
-    institutionNumber: Number,
-    email: String,
-  })
-);
+// Define the schema for the Payment Details model
+const paymentDetailsSchema = new mongoose.Schema({
+  mentorId: String,
+  accountNumber: Number,
+  transitNumber: Number,
+  institutionNumber: Number,
+  email: String,
+});
 
+// Create the Payment Details model
+const PaymentDetails = mongoose.model("Payment", paymentDetailsSchema);
+
+// Export the Payment Details model
 export default PaymentDetails;

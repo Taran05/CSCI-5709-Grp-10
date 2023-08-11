14fa0a1ce289b7a7ec6eb98cc3b5bb45cdc88294
Taranjot Singh <tr548284@dal.ca>
2023-08-11 19:09:57 -0300

    fixed: 🐛responsiveness of availability-calendar page

--- Frontend/src/Components/availability-calendar/availability-calendarComp.jsx
+++ Frontend/src/Components/availability-calendar/availability-calendarComp.jsx
@@ -1,122 +1,112 @@
-/**
- * This component displays an availability calendar and schedule options.
- * Users can switch between the schedule and calendar view.
- *
- * Author: Taranjot Singh <tr548284@dal.ca/B00945917>
- */
-import "./availability-calendarComp.css";
-import React, { useState } from "react";
-import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
-import ScheduleComp from "../../Components/availability-calendar/schedule/scheduleComp";
-import CalendarComp from "../../Components/availability-calendar/calendar/calendarComp";
-import { Button, Grid } from "@mui/material";
-import { grey } from "@mui/material/colors";
-import UseMediaQuery from "@mui/material/useMediaQuery";
-
-// Define responsive breakpoints using MUI's createTheme
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 330,
-      sm: 380,
-      md: 430,
-      lg: 1100,
-      xl: 1450,
+  /**
+ * @author Taranjot Singh <tr548284@dal.ca/B00945917>
+ */ 
+  import "./availability-calendarComp.css";
+  import React, { useState } from "react";
+  import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+  import ScheduleComp from "../../Components/availability-calendar/schedule/scheduleComp";
+  import CalendarComp from "../../Components/availability-calendar/calendar/calendarComp";
+  import { Button, Grid } from "@mui/material";
+  import { grey } from "@mui/material/colors";
+  import UseMediaQuery from "@mui/material/useMediaQuery";
+  
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 330,
+        sm: 380,
+        md: 430,
+        lg: 1100,
+        xl: 1450,
+      },
     },
-  },
-});
-
-// Styled components for Schedule and Calendar buttons
-const ScheduleButton = styled(Button)(({ theme, showSelected }) => ({
-  // Common styles for the buttons
-  height: "100%",
-  padding: "10px 30px",
-  fontWeight: 600,
-  borderRadius: "50px",
-  pointerEvents: showSelected ? "none" : "auto",
-  color: showSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
-  backgroundColor: showSelected ? "#1D267D" : grey[50],
-  "&:hover": {
-    backgroundColor: showSelected ? "none" : "#D4ADFC",
-  },
-}));
-
-// Styled component for the Calendar button
-const CalendarButton = styled(Button)(({ theme, showSelected }) => ({
-  // Common styles for the buttons
-  height: "100%",
-  padding: "10px 30px",
-  fontWeight: 600,
-  borderRadius: "50px",
-  pointerEvents: showSelected ? "none" : "auto",
-  color: showSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
-  backgroundColor: showSelected ? "#1D267D" : grey[50],
-  "&:hover": {
-    backgroundColor: showSelected ? "none" : "#D4ADFC",
-  },
-}));
-
-export default function AvailabilityCalendar() {
-  // State variables to manage the selected component
-  const [showSchedule, setShowSchedule] = useState(true);
-  const isSmallScreen = UseMediaQuery((theme) => theme.breakpoints.down("sm"));
-
-  // Event handler for when the Schedule button is clicked
-  const handleSchedule = () => {
-    setShowSchedule(true);
-  };
-
-  // Event handler for when the Calendar button is clicked
-  const handleCalendar = () => {
-    setShowSchedule(false);
-  };
-
-  return (
-    <div className="Calendar">
-      <div className="calendar-container">
-        <div className="left-schedule">
-          <div className="calendar-form">
-            <h1 className="calendar-form-header">AVAILABILITY</h1>
-            <Grid item sm={12}>
-              <Grid container spacing={2}>
-                <Grid item sm={0}>
-                  {/* Schedule Button */}
-                  <ScheduleButton
-                    variant="contained"
-                    onClick={handleSchedule}
-                    fullWidth
-                    showSelected={showSchedule}
-                    style={isSmallScreen ? { width: "130px", height: "50px", fontSize: "12px" } : {}}
-                    className="schedule-button"
-                  >
-                    Schedule
-                  </ScheduleButton>
-                </Grid>
-                <Grid item sm={0}>
-                  {/* Calendar Button */}
-                  <CalendarButton
-                    variant="contained"
-                    onClick={handleCalendar}
-                    fullWidth
-                    showSelected={!showSchedule}
-                    style={isSmallScreen ? { width: "130px", height: "50px", fontSize: "12px" } : {}}
-                    className="calendar-button"
-                  >
-                    Calendar Settings
-                  </CalendarButton>
+  });
+  
+  const ScheduleButton = styled(Button)(({ theme, showSelected }) => ({
+    height: "100%",
+    padding: "10px 30px", 
+    fontWeight: 600,
+    borderRadius: "50px",
+    pointerEvents: showSelected ? "none" : "auto",
+    color: showSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
+    backgroundColor: showSelected ? "#1D267D" : grey[50],
+    "&:hover": {
+      backgroundColor: showSelected ? "none" : "#D4ADFC",
+    },
+  }));
+  
+  const CalendarButton = styled(Button)(({ theme, showSelected }) => ({
+    height: "100%",
+    padding: "10px 30px", 
+    fontWeight: 600,
+    borderRadius: "50px",
+    pointerEvents: showSelected ? "none" : "auto",
+    color: showSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
+    backgroundColor: showSelected ? "#1D267D" : grey[50],
+    "&:hover": {
+      backgroundColor: showSelected ? "none" : "#D4ADFC",
+    },
+  }));
+  
+  export default function AvailabilityCalendar() {
+    // State variables to manage the selected component
+    const [showSchedule, setShowSchedule] = useState(true);
+    const isSmallScreen = UseMediaQuery((theme) => theme.breakpoints.down("sm"));
+  
+    // Event handler for when the Schedule button is clicked
+    const handleSchedule = () => {
+      setShowSchedule(true);
+    };
+  
+    // Event handler for when the Calendar button is clicked
+    const handleCalendar = () => {
+      setShowSchedule(false);
+    };
+  
+    return (
+      <div className="Calendar">
+        <div className="calendar-container">
+          <div className="left-schedule">
+            <div className="calendar-form">
+              <h1 className="calendar-form-header">AVAILABILITY</h1>
+              <Grid item sm={12}>
+                <Grid container spacing={2}>
+                  <Grid item sm={0}>
+                    <ScheduleButton
+                      variant="contained"
+                      onClick={handleSchedule}
+                      fullWidth
+                      showSelected={showSchedule}
+                      style= {isSmallScreen ? {width : "130px", height : "50px", fontSize : "12px"} : {} }
+                      className="schedule-button"
+                    >
+                      Schedule
+                    </ScheduleButton>
+                  </Grid>
+                  <Grid item sm={0}>
+                    <CalendarButton
+                      variant="contained"
+                      onClick={handleCalendar}
+                      fullWidth
+                      showSelected={!showSchedule}
+                      style= {isSmallScreen ? {width : "130px", height : "50px", fontSize : "12px"} : {} }
+                      className="calendar-button"
+                    >
+                      Calendar Settings
+                    </CalendarButton>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            <br />
-            <hr />
-            <br />
-            {/* Conditionally render either ScheduleComp or CalendarComp */}
-            <ThemeProvider theme={theme}>
-              {showSchedule ? <ScheduleComp /> : <CalendarComp />}
-            </ThemeProvider>
+              <br></br>
+              <hr />
+              <br></br>
+              <ThemeProvider theme={theme}>
+                {showSchedule ? <ScheduleComp /> : <CalendarComp />}
+              </ThemeProvider>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
--- Frontend/src/Components/availability-calendar/schedule/scheduleComp.jsx
+++ Frontend/src/Components/availability-calendar/schedule/scheduleComp.jsx
@@ -1,109 +1,99 @@
-/**
- * This component handles the schedule selection and rendering of different schedule components.
- * Author: Taranjot Singh <tr548284@dal.ca/B00945917>
- */
-import React, { useState } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
-import { Button, Grid } from "@mui/material";
-import { grey } from "@mui/material/colors";
-import DefaultScheduleComp from "./defaultschedule/defaultscheduleComp";
-import BlockDatesComp from "./blockdates/blockdatesComp";
-import AlternateScheduleComp from "./alternateschedule/alternatescheduleComp";
-
-export default function Schedule() {
-  // Create a theme for responsive design
-  const theme = createTheme({
-    breakpoints: {
-      values: {
-        xs: 330,
-        sm: 380,
-        md: 430,
-        lg: 1100,
-        xl: 1450,
+  /**
+ * @author Taranjot Singh <tr548284@dal.ca/B00945917>
+ */ 
+  import React, { useState } from "react";
+  import "react-toastify/dist/ReactToastify.css";
+  import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+  import { Button, Grid } from "@mui/material";
+  import { grey } from "@mui/material/colors";
+  import DefaultScheduleComp from "./defaultschedule/defaultscheduleComp";
+  import BlockDatesComp from "./blockdates/blockdatesComp";
+  import AlternateScheduleComp from "./alternateschedule/alternatescheduleComp";
+  
+  export default function Schedule() {
+  
+    const theme = createTheme({
+      breakpoints: {
+        values: {
+          xs: 330,
+          sm: 380,
+          md: 430,
+          lg: 1100,
+          xl: 1450,
+        },
       },
-    },
-  });
-
-  // State variable to track the selected schedule type
-  const [selectedSchedule, setSelectedSchedule] = useState("Default");
-
-  // Styled component for the Default Schedule button
-  const DefaultScheduleButton = styled(Button)(({ theme, isSelected }) => ({
-    height: "100%",
-    width: "100%",
-    fontWeight: 600,
-    pointerEvents: isSelected ? "none" : "auto",
+    });
+  
+    const [selectedSchedule, setSelectedSchedule] = useState("Default");
+  
+    const DefaultScheduleButton = styled(Button)(({ theme, isSelected }) => ({
+      height: "100%",
+      width: "100%",
+      fontWeight: 600,
+      pointerEvents: isSelected ? "none" : "auto",
     color: isSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
     backgroundColor: isSelected ? "#1D267D" : grey[50],
     "&:hover": {
       backgroundColor: isSelected ? "none" : "#D4ADFC",
     },
-  }));
-
-  // Styled component for the Alternate Schedule button
-  const AlternateScheduleButton = styled(Button)(({ theme, isSelected }) => ({
-    height: "100%",
-    width: "100%",
-    fontWeight: 600,
-    pointerEvents: isSelected ? "none" : "auto",
-    color: isSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
-    backgroundColor: isSelected ? "#1D267D" : grey[50],
-    "&:hover": {
-      backgroundColor: isSelected ? "none" : "#D4ADFC",
-    },
-  }));
-
-  // Component rendering
-  return (
-    <>
-      {/* Schedule Selection Buttons */}
-      <Grid item sm={12}>
-        <Grid container spacing={2}>
-          <Grid item sm={0}>
+    }));
+  
+    const AlternateScheduleButton = styled(Button)(({ theme, isSelected }) => ({
+      height: "100%",
+      width: "100%",
+      fontWeight: 600,
+      pointerEvents: isSelected ? "none" : "auto",
+      color: isSelected ? theme.palette.getContrastText(grey[900]) : theme.palette.getContrastText(grey[50]),
+      backgroundColor: isSelected ? "#1D267D" : grey[50],
+      "&:hover": {
+        backgroundColor: isSelected ? "none" : "#D4ADFC",
+      },
+    }));
+  
+    return (
+      <>
+        <Grid item sm={12}>
+          <Grid container spacing={2}>
+            <Grid item sm={0}>
             <DefaultScheduleButton
-              variant="contained"
-              fullWidth
-              onClick={() => setSelectedSchedule("Default")}
-              isSelected={selectedSchedule === "Default"}
-            >
-              Default
-            </DefaultScheduleButton>
-          </Grid>
-          <Grid item sm={0}>
+                variant="contained"
+                fullWidth
+                onClick={() => setSelectedSchedule("Default")}
+                isSelected={selectedSchedule === "Default"}
+              >
+                Default
+              </DefaultScheduleButton>
+            </Grid>
+            <Grid item sm={0}>
             <AlternateScheduleButton
-              variant="contained"
-              fullWidth
-              onClick={() => setSelectedSchedule("Alternate")}
-              isSelected={selectedSchedule === "Alternate"}
-            >
-              Alternate
-            </AlternateScheduleButton>
+                variant="contained"
+                fullWidth
+                onClick={() => setSelectedSchedule("Alternate")}
+                isSelected={selectedSchedule === "Alternate"}
+              >
+                Alternate
+              </AlternateScheduleButton>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-
-      <br></br>
-      <br></br>
-      <br></br>
-
-      {/* Rendering Schedule Components */}
-      <Grid container spacing={2}>
-        {selectedSchedule === "Default" ? (
-          <Grid item sm={6}>
-            <ThemeProvider theme={theme}><DefaultScheduleComp /></ThemeProvider>
-          </Grid>
-        ) : (
-          <Grid item sm={6}>
-            <ThemeProvider theme={theme}><AlternateScheduleComp /></ThemeProvider>
-          </Grid>
-        )}
-
-        {/* Always render BlockDatesComp */}
-        <Grid item sm={6}>
+  
+        <br></br>
+        <br></br>
+        <br></br>
+        <Grid container spacing={2}>
+          {selectedSchedule === "Default" ? (
+            <Grid item sm={6}>
+              <ThemeProvider theme={theme}><DefaultScheduleComp /></ThemeProvider>
+            </Grid>
+          ) : (
+            <Grid item sm={6}>
+              <ThemeProvider theme={theme}><AlternateScheduleComp /></ThemeProvider>
+            </Grid>
+          )}
+          <Grid>
           <ThemeProvider theme={theme}><BlockDatesComp /></ThemeProvider>
+          </Grid>
         </Grid>
-      </Grid>
-    </>
-  );
-}
+      </>
+    );
+  }
